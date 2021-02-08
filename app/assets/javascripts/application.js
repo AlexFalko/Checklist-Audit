2ec4f64f790c41d3699de115f7a47af989b1c663
Alexander <sashafalko129@gmail.com>
2021-02-08 15:19:14 +0200

    Delete js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,22 +16,6 @@
 //= require turbolinks
 
 
-const addBook = ()=> {
-  const createButton = document.getElementById("addBook");
-  createButton.addEventListener("click", () => {
-
-    const lastId = document.querySelector('#fieldsetContainer').lastElementChild.id;
-
-    const newId = parseInt(lastId, 10) + 1;
-
-    const newFieldset = document.querySelector('[id="0"]').outerHTML.replace(/0/g,newId);
-
-    document.querySelector("#fieldsetContainer").insertAdjacentHTML(
-        "beforeend", newFieldset
-    );
-  });
-}
-export { addBook }
 
 
 
