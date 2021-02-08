9361971dbe4a7d59f71066c6ce8e05d296336f60
Alexander <sashafalko129@gmail.com>
2021-02-08 16:04:25 +0200

    Add seeds

--- app/models/response.rb
+++ app/models/response.rb
@@ -1,5 +1,9 @@
 class Response < ApplicationRecord
+  MIN_LENGTH_DESCRIPTION = 12
   enum answer: { yes: 1, no: 0, no_data: 2 }
   belongs_to :question
   belongs_to :audit
+
+  validates :answer, :description, presence: true
+  validates :description, length: { minimum: MIN_LENGTH_DESCRIPTION }
 end
