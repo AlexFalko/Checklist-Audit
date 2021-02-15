25f7cece651cdeab239a0c62d738bedd9c9bff6b
Alexander <sashafalko129@gmail.com>
2021-02-15 23:39:34 +0200

    change seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,12 @@
-User.create(
-        email: "user@gmail.com",
-        password: "123456"
-    )
+# User.create(
+#         email: "user@gmail.com",
+#         password: "123456"
+#     )
 
 20.times do |i|
  checklist = Checklist.create(title: FFaker::Book.title, description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod ', user_id: 1)
  Audit.create(checklist: checklist, user_id: 1)
- Question.create(title: FFaker::Book.title, description: 'Lorem ipsum',checklist: checklist, user_id: 1 )
+ Question.create(title: FFaker::Book.title, description: 'Lorem ipsum',checklist: checklist)
 end
 
 
