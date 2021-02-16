d887cbb599261d43496ca221d60abf3e5e140826
Alexander <sashafalko129@gmail.com>
2021-02-16 21:21:09 +0200

    Add validate User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,13 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :validatable
+  MAX_LENGTH_EMAIL = 50
+  MAX_LENGTH_PASSWORD = 50
+
+  validates :email, presence: true, length: { maximum: MAX_LENGTH_EMAIL }
+  validates :password, presence: true, length: { maximum: MAX_LENGTH_PASSWORD }
+
   has_many :checklists
   has_many :audits
+
+  
 end
